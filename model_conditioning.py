eadc76a8bd82fc3da98b5364be07885c19d4e2b7
Kate Hu <katehu310@Kates-MacBook-Pro-2.local>
2023-04-23 20:05:19 -0400

    add note

--- model_conditioning.py
+++ model_conditioning.py
@@ -8,6 +8,7 @@ from torch.utils.data import DataLoader
 class ConditioningEncoder(nn.Module):
     def __init__(self, encoder_name, embed_dim):
         super(ConditioningEncoder, self).__init__()
+        # images need to be rescaled to [0.0, 1.0] and then normalized using mean=[0.485, 0.456, 0.406] and std=[0.229, 0.224, 0.225]
         if encoder_name == 'vgg':
             self.vgg = torchvision.models.vgg11(weights='DEFAULT')
             self.model = nn.Sequential(
